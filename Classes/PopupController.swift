5279e05833254e68ecf7625837bf5bdb95edef99
yowlu <info@yowlu.com>
2019-06-02 17:40:44 +0200

    Update PopupController.swift

--- Classes/PopupController.swift
+++ Classes/PopupController.swift
@@ -33,7 +33,7 @@ public protocol PopupContentViewController {
 
 open class PopupController: UIViewController {
     
-    open var hasSafeArea: Bool {
+    static var hasSafeArea: Bool {
         guard #available(iOS 11.0, *), let topPadding = UIApplication.shared.keyWindow?.safeAreaInsets.top, topPadding > 24 else {
             return false
         }
